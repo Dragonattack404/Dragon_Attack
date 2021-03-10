0bf6c7cec4bbeda46c622e67fc204956e873e619
we are (Tm~DA) Dragon_Attack <80425813+Dragonattack404@users.noreply.github.com>
2021-03-10 22:47:23 +0200

    Add files via upload

---
+++ Dragon_Attack.py
@@ -0,0 +1,70 @@
+
+import os,sys,time
+import webbrowser
+import time
+def jalan(z):
+ for e in z + '\n':
+  sys.stdout.write(e)
+  sys.stdout.flush()
+  time.sleep(0000000.02)
+jalan ("""\033[1;33m ########## Welcome To Script Dragon_Attack ########## 
+ \033[1;34m
+Telegram: http://t.me/nettfree0
+  
+\033[1;32m
+
+
+██████╗░██████╗░░█████╗░░██████╗░░█████╗░███╗░░██╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝░██╔══██╗████╗░██║
+██║░░██║██████╔╝███████║██║░░██╗░██║░░██║██╔██╗██║
+██║░░██║██╔══██╗██╔══██║██║░░╚██╗██║░░██║██║╚████║
+██████╔╝██║░░██║██║░░██║╚██████╔╝╚█████╔╝██║░╚███║
+╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝░╚═════╝░░╚════╝░╚═╝░░╚══╝
+
+░█████╗░████████╗████████╗░█████╗░░█████╗░██╗░░██╗
+██╔══██╗╚══██╔══╝╚══██╔══╝██╔══██╗██╔══██╗██║░██╔╝
+███████║░░░██║░░░░░░██║░░░███████║██║░░╚═╝█████═╝░
+██╔══██║░░░██║░░░░░░██║░░░██╔══██║██║░░██╗██╔═██╗░
+██║░░██║░░░██║░░░░░░██║░░░██║░░██║╚█████╔╝██║░╚██╗
+╚═╝░░╚═╝░░░╚═╝░░░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝
+\033[1;31m
+######### By/Dragon_Attack ##########
+###### script Vodafone Extra #######
+ """)
+link1="http://www.goolge.com"
+link2="https://fast.com/ar/"
+link3="https://www.kali.org/downloads/"
+link4="https://ubuntu.com/"
+link5="https://www.parrotsec.org/"
+link6="https://www.fekr3h.com/2021/03/btc.html"
+link7="https://www.fekr3h.com/2021/03/2giga.html"
+link8="https://github.com/"
+link9="https://www.netflix.com/eg/"
+link10="https://back.egybest.co/movies/2020"
+link11="https://en.wikipedia.org/wiki/Crack_cocaine"
+result=0
+print(' ')
+Numper=int(input('\033[1;33mEnter the number of points: '))
+
+print(' ')
+
+webbrowser.open(link1)
+while True:
+ result=result+10
+ if Numper==result:
+  print(' ')
+  print("\033[1;34m-Done successfully" +" "+ "\033[1;32m-Points" ,Numper )
+  break
+ print("\033[1;37m-Done add 10 points..")
+ time.sleep(20)
+ webbrowser.open(link1)
+ webbrowser.open(link2)
+ webbrowser.open(link3)
+ webbrowser.open(link4)
+ webbrowser.open(link5)
+ webbrowser.open(link6)
+ webbrowser.open(link7) 
+ webbrowser.open(link8)
+ webbrowser.open(link9)
+ webbrowser.open(link10)
+ webbrowser.open(link11)
